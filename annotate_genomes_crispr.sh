006e6e864894dee98c09300431c09ceb0a7bfe6b
payya4 <167255490+payya4@users.noreply.github.com>
2024-07-23 16:56:00 +0100

    Update annotate_genomes_crispr.sh

--- annotate_genomes_crispr.sh
+++ annotate_genomes_crispr.sh
@@ -17,7 +17,8 @@ source /gpfs01/home/payya4/miniconda3/etc/profile.d/conda.sh
 # Activate the conda environment
 conda activate /gpfs01/home/payya4/miniconda3/envs/prokka_env   
 
-# Define an associative array to map organism names to their respective kingdom, genus, and species
+# Define an associative array to map organism names to their respective kingdom, genus, and species.
+#List co-piloted by AI to expedite the process of listing this information   
 declare -A organism_info
 organism_info=(
     ["Acetobacter_aceti"]="Bacteria Acetobacter aceti"
@@ -138,3 +139,4 @@ done
 # Deactivate conda
 conda deactivate
 
+#script made by Yasmine Alqanai with the exception of the list commented declaring the use of AI.
